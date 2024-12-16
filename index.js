ff7daadffc55875c0f6d26c250e4bf4cfaa81add
adityakumarsahu108 <adityakumarsahu108@gmail.com>
2024-12-16 23:34:15 +0530

    final changes

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
-require('dotenv').config();
 // const fs = require("fs");
 // const path = require("path");
+console.log(process.env); // Log environment variables
 
 // Initialize Firebase Admin
 const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // Ensure proper newline in private key
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
